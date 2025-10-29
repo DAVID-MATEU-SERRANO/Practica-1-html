0ca21bf05a6b146c477457723e52c07bb9daa87c
Manuel Manchado Barquero <100522228@alumnos.uc3m.es>
2025-10-29 18:41:26 +0100

    Carrusel en página compras

--- ej/script/carrusel.js
+++ ej/script/carrusel.js
@@ -2,6 +2,9 @@ const track = document.querySelector(".pack-info");
 const items = document.querySelectorAll(".carrusel-item");
 const prevBtn = document.querySelector(".nav-button-1");
 const nextBtn = document.querySelector(".nav-button-2");
+const boton1 = document.querySelector(".pack-info-button-0");
+const boton2 = document.querySelector(".pack-info-button-1");
+const boton3 = document.querySelector(".pack-info-button-2");
 
 let index = 0;
 let timer;
@@ -24,6 +27,43 @@ function showSlide(n) {
 
 
 
+boton1.addEventListener("click", () => {
+  let compra_actual = JSON.parse(localStorage.getItem("compra_actual"));
+  if (!compra_actual) {
+    compra_actual = {};
+  }
+  compra_actual.pack = "Pack Sudesde Asiático";
+  compra_actual.precio = 600;
+  compra_actual.subtitulo = "Indonesia: buses, hostales y guia de visados";
+  compra_actual.imagen = "images/img_pack.jpg";
+  compra_actual.descripcion = "Este pack está pensado para mochileros que quieren recorrer el Sudeste Asiático con libertad y buen ritmo." +
+                        "Incluye transporte en buses locales para moverte entre ciudades y pueblos, alojamiento en hostales seleccionados donde podrás conocer a otros viajeros y una guía práctica de visados que te ahorrará tiempo en frontera." +
+                        "Con un presupuesto de 600 €, tendrás lo esencial para explorar templos, playas y volcanes, disfrutando del día a día como un local. " +
+                        "El enfoque es flexible: tú decides cuántos días quedarte en cada lugar y qué experiencias añadir. "+
+                        "Indonesia te espera con su mezcla de naturaleza, cultura y hospitalidad: este pack es tu punto de partida.";
+  localStorage.setItem("compra_actual", JSON.stringify(compra_actual));
+  window.location.href = "versionC.html";
+});
+
+boton2.addEventListener("click", () => {
+  if (!compra_actual) {
+    compra_actual = {};
+  }
+  compra_actual.pack = "Pack Sudesde Asiático";
+  compra_actual.precio = 600;
+  compra_actual.subtitulo = "Indonesia: buses, hostales y guia de visados";
+  compra_actual.imagen = "images/img_pack.jpg";
+  compra_actual.descripcion = "Este pack está pensado para mochileros que quieren recorrer el Sudeste Asiático con libertad y buen ritmo." +
+                        "Incluye transporte en buses locales para moverte entre ciudades y pueblos, alojamiento en hostales seleccionados donde podrás conocer a otros viajeros y una guía práctica de visados que te ahorrará tiempo en frontera." +
+                        "Con un presupuesto de 600 €, tendrás lo esencial para explorar templos, playas y volcanes, disfrutando del día a día como un local. " +
+                        "El enfoque es flexible: tú decides cuántos días quedarte en cada lugar y qué experiencias añadir. "+
+                        "Indonesia te espera con su mezcla de naturaleza, cultura y hospitalidad: este pack es tu punto de partida.";
+  localStorage.setItem("compra_actual", JSON.stringify(compra_actual));
+  window.location.href = "versionC.html";
+}); 
+boton3.addEventListener("click", () => {
+  window.location.href = "versionC.html";
+});
 
 
 prevBtn.addEventListener("click", () => {
